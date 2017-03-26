8d3a0888ceece10a10ecbe9a08ccd423af3e6792
SsnL <tongzhou.wang.1994@gmail.com>
2017-03-26 00:14:10 -0700

    fix

--- i3/experiments/experiment1.py
+++ i3/experiments/experiment1.py
@@ -183,7 +183,7 @@ def create_jobs(num_jobs):
 def create_reference_jobs(num_jobs_per_case):
   jobs = []
   seed = 1000
-  net_names = ["triangel-n120"] #["75-25-{}".format(i) for i in xrange(2, 3)]
+  net_names = ["triangle-n120"] #["75-25-{}".format(i) for i in xrange(2, 3)]
   determinisms = [99]
   prior_ratios = [1.]
   max_inverse_sizes = [20]
